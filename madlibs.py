a786ce9b223875202041c30c69be8c506ad0d4a2
bimashazaman <bimashazaman1234@gmail.com>
2022-02-14 21:40:06 +0600

    yayyy i'm pro now

---
+++ madlibs.py
@@ -0,0 +1,22 @@
+# youtuber = "bimasha"
+
+# print("Subscribe to " + youtuber + " on YouTube!")
+# print("Subscribe to {} ".format(youtuber) + " on YouTube!")
+
+
+adj = input("Adjective: ")
+verb_1 = input("Verb: ")
+noun_1 = input("Noun: ")
+
+
+madlib = f"computer programmin is so {adj} that it {verb_1} and {noun_1}!"
+
+print(madlib)
+
+# from sample_madlib import *
+
+# if __name__ == "__main__":
+#    m = random.choice([hp, code, zombie, madlib])
+#    m.madlib()
+
+   
